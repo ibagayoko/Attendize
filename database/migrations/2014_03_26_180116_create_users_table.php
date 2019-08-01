a6832d252a6159b21a08431fc299fe4a97555ab6
Ismail bagayoko <bagayoko.ismail@gmail.com>
2019-08-01 09:50:07 -0300

    CEANING WIP

--- database/migrations/2014_03_26_180116_create_users_table.php
+++ database/migrations/2014_03_26_180116_create_users_table.php
@@ -263,10 +263,17 @@ class CreateUsersTable extends Migration
             $t->unsignedInteger('order_status_id');
             $t->nullableTimestamps();
             $t->softDeletes();
-
+            
             $t->string('first_name');
             $t->string('last_name');
             $t->string('email');
+            $t->string('business_name')->nullable();
+            $t->string('business_tax_number')->nullable();
+            $t->string('business_address_line_one')->nullable();
+            $t->string('business_address_line_two')->nullable();
+            $t->string('business_address_state_province')->nullable();
+            $t->string('business_address_city')->nullable();
+            $t->string('business_address_code')->nullable();
             $t->string('ticket_pdf_path', 155)->nullable();
 
             $t->string('order_reference', 15);
@@ -283,6 +290,9 @@ class CreateUsersTable extends Migration
             $t->boolean('is_partially_refunded')->default(0);
             $t->boolean('is_refunded')->default(0);
             $t->boolean('is_payment_received')->default(0);
+            $t->boolean('is_business')->default(false);
+
+            
 
             $t->decimal('amount', 13, 2);
             $t->decimal('amount_refunded', 13, 2)->nullable();
@@ -290,6 +300,7 @@ class CreateUsersTable extends Migration
 
             $t->unsignedInteger('event_id')->index();
             $t->unsignedInteger('payment_gateway_id')->nullable();
+
 
             $t->foreign('payment_gateway_id')->references('id')->on('payment_gateways');
             $t->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
